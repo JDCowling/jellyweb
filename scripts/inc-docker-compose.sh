f190e6d2f6bf90d215509be8f78e257a4e68c270
Jack Cowling <j.d.cowling0398@gmail.com>
2023-09-30 21:23:01 +0100

    reverted the ports

--- scripts/inc-docker-compose.sh
+++ scripts/inc-docker-compose.sh
@@ -20,8 +20,8 @@ services:
     dockerfile: Dockerfile
    container_name: jellyweb-http
    ports:
-    - 80:14952
-    - 443:14953
+    - 14952:80
+    - 14953:443
    networks:
     - jellyweb-net
    restart: 'unless-stopped'
